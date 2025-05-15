612e302e05ae49d290580427dfc4982e70f35958
Kardnit <a.borgaiskender@gmail.com>
2025-05-15 18:51:12 +0200

    Updated pages
    Updated pages

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div class="h-full w-full flex items-end justify-end">
-      <div class="text-right max-w-xs lg:max-w-xl text-4xl lg:text-8xl pointer-events-auto">
+      <div class="text-right max-w-xs lg:max-w-xl text-4xl lg:text-7xl pointer-events-auto">
         I believe software development can bridge human needs with technological
         possibilities.
       </div>
--- src/routes/info.tsx
+++ src/routes/info.tsx
@@ -1,7 +1,7 @@
 export default function Info() {
   return (
     <div class="h-full w-full flex items-end justify-end">
-      <div class="text-right max-w-xs lg:max-w-xl text-4xl lg:text-8xl pointer-events-auto">
+      <div class="text-right max-w-xs lg:max-w-xl text-4xl lg:text-7xl pointer-events-auto">
         <a
           href="https://www.linkedin.com/in/atilla-iskender-400685253/"
           target="_blank"
